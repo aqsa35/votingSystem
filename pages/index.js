b87fc557be95f9b392bc87e768c376753e079936
Aqsa Iftikhar <iftiaqsa357@gmail.com>
2024-01-25 03:49:40 -0800

    voting

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React,{ useState,useEffect,useContext} from "react";
+import  Image  from "next/image";
+import Countdown from "react-countdown";
+//internal import
+import { VotingContext } from "../context/voter";
+import Style from "../styles/index.module.css";
+import Card from "../components/Card/Card";
+import image from "./panda.jpg";   
+const index = ()=>{
+    const{votingTitle}= useContext(VotingContext)
+    return <div>{votingTitle}</div>;
+};
+export default index;
